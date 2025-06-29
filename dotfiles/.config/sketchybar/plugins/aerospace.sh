1ca48dd9797d4d052a0c527e8518842ab56f05b4
albamr09 <bertaalba1@gmail.com>
2025-06-29 16:09:26 +0200

    [Sketchybar] Improve workspace styling

--- dotfiles/.config/sketchybar/plugins/aerospace.sh
+++ dotfiles/.config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME background.border_width=2 label.drawing=on
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME background.border_width=1 label.drawing=off
 fi
