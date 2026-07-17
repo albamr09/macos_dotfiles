a3a53acb674715e5ed742184ab0002e47bbebb3c
albamr09 <bertaalba1@gmail.com>
2026-07-17 08:15:34 +0200

    Add more treesitter parsers :)

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -53,7 +53,7 @@ require("oil").setup({
 -----------------------------------
 -- Treesitter
 -----------------------------------
-require('nvim-treesitter').install { 'c', 'javascript', 'jsx', 'tsx', 'typescript', 'python', 'json', 'java', 'xml', 'html', 'lua', 'bash', 'dockerfile', 'cpp', 'css' }
+require('nvim-treesitter').install { 'c', 'javascript', 'jsx', 'tsx', 'typescript', 'python', 'json', 'java', 'xml', 'html', 'lua', 'bash', 'dockerfile', 'cpp', 'css', 'toml', 'dart' }
 
 -- Only highlight with tree-sitter
 vim.cmd("syntax off")
